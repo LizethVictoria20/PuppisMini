3781c5f7032e6dcf542532f111da37d6ef83589c
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-13 16:05:28 -0500

    Renderizando componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import Menu from "./components/menu";
+import Home from "./components/home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Menu
+          avatar={{
+            url:
+              "https://instagram.feoh4-2.fna.fbcdn.net/vp/4d3f0800aa5095950d6b7f5845840d50/5D191DE5/t51.2885-15/e35/45643783_734780323575509_3152925887313704723_n.jpg?_nc_ht=instagram.feoh4-2.fna.fbcdn.net",
+            alt: "Avatar"
+          }}
+        />
+        <Home
+          front={{
+            url:
+              "https://instagram.feoh4-2.fna.fbcdn.net/vp/4d3f0800aa5095950d6b7f5845840d50/5D191DE5/t51.2885-15/e35/45643783_734780323575509_3152925887313704723_n.jpg?_nc_ht=instagram.feoh4-2.fna.fbcdn.net",
+            alt: "front"
+          }}
+        />
       </div>
     );
   }
