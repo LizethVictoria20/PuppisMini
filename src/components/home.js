f61d5589503f4456fbd7ad9a736d6921ef57bce1
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-13 16:34:37 -0500

    Estilos de home

--- src/components/home.js
+++ src/components/home.js
@@ -2,23 +2,21 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const ImagenFront = styled.img`
-  border-radius: 60%;
-  width: 30%;
+  border-radius: 50%;
+  width: 20%;
   object-fit: cover;
   float: right;
+`;
+const Title = styled.h1`
+  font-size: 50px;
 `;
 
 class Home extends Component {
   render() {
     return (
       <div className="name d-sm-flex justify-content-center align-items-center flex-column iam-side">
-        <figure
-          className='d-sm-flex justify-content-center align-items-center flex-column iam-side">
-          <section class="navbar-'
-        >
-          <ImagenFront src={this.props.front.url} alt={this.props.front.alt} />
-        </figure>
-        <h1 className="text-center">PUPPIS MINI</h1>
+        <ImagenFront src={this.props.front.url} alt={this.props.front.alt} />
+        <Title>PUPPIS MINI</Title>
         <h3>Cachorros Miniatura</h3>
       </div>
     );
